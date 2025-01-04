c38673d3bdb132e94600280090ea3cb94d397b08
Watchamacall <ryanpc123@gmail.com>
2025-01-04 20:58:06 +0000

    Added keybinds
    Made strides to make a working keybind system

---
+++ Asteroids/KeyBindings.cpp
@@ -0,0 +1,27 @@
+#include "KeyBindings.h"
+
+void KeyBindings::AddKeyBinding(const std::string& keyName, char key)
+{
+	//Adds the char as the key to the Binding Name
+	keyBindings[key] = keyName;
+}
+
+void KeyBindings::AddDelegateToKeyBinding(const std::string& keyName, const std::function<void>& delegate)
+{
+	//pushes back the delegate ready for calling
+	actionToDelegate[keyName].push_back(delegate);
+}
+
+void KeyBindings::HandleInput(char key)
+{
+	if (keyBindings.find(key) != keyBindings.end())
+	{
+		if (actionToDelegate.find(keyBindings[key]) != actionToDelegate.end())
+		{
+			for (const VoidDelegate& aDelegate : actionToDelegate[keyBindings[key]])
+			{
+				aDelegate();
+			}
+		}
+	}
+}
---
+++ Asteroids/KeyBindings.h
@@ -0,0 +1,33 @@
+#pragma once
+#define VoidDelegate std::function<void>
+
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <functional>
+#include <memory>
+
+class KeyBindings
+{
+private:
+	/*
+	* Maps a key to a string
+	* char = Keyboard Key Pressed
+	* std::string = Name of Binding
+	*/
+	std::unordered_map<char, std::string> keyBindings;
+	/*
+	* Maps a Binding to a vector of Delegates
+	* std::string = Binding Name
+	* std::vector<VoidDelegate> = Delegates to be called when Binding is pressed
+	*/
+	std::unordered_map<std::string, std::vector<VoidDelegate>> actionToDelegate; //vector is of unfixed size, unlike []
+
+public:
+	void AddKeyBinding(const std::string& keyName, char key);
+
+	void AddDelegateToKeyBinding(const std::string& keyName, const std::function<void>& delegate);
+
+	void HandleInput(char key);
+};
+
